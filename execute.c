64eb33a3f2f8c26e8d41f4fe8b26fede13937370
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-03-23 22:42:08 -0700

    update execute func

--- execute.c
+++ execute.c
@@ -11,8 +11,9 @@
 
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
-	instruction_t opst[] = {
-				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+	instruction_t opst[] = {{"push", f_push},
+				{"pall", f_pall},
+				{"pint", f_pint},
 				{"pop", f_pop},
 				{"swap", f_swap},
 				{"add", f_add},
@@ -27,8 +28,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"rotr", f_rotr},
 				{"queue", f_queue},
 				{"stack", f_stack},
-				{NULL, NULL}
-				};
+				{NULL, NULL}};
 	unsigned int i = 0;
 	char *op;
 
@@ -36,13 +36,12 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	if (op && op[0] == '#')
 		return (0);
 	bus.arg = strtok(NULL, " \n\t");
-	while (opst[i].opcode && op)
+	for (; opst[i].opcode && op; i++)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
 		{	opst[i].f(stack, counter);
 			return (0);
 		}
-		i++;
 	}
 	if (op && opst[i].opcode == NULL)
 	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
